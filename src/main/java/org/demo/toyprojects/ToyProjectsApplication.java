06fab7fb9b49590376c82886dcfc97a058cefde2
s2706172 <y.wu-7@student.utwente.nl>
2024-10-07 21:40:10 +0200

    init

---
+++ src/main/java/org/demo/toyprojects/ToyProjectsApplication.java
@@ -0,0 +1,13 @@
+package org.demo.toyprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyProjectsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ToyProjectsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/demo/toyprojects/ToyProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.demo.toyprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyProjectsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
