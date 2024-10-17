fb66d3d402a018c71d065d5388b0832b310063cd
s2706172 <y.wu-7@student.utwente.nl>
2024-10-17 15:37:18 +0200

    [ADD] api gateway

---
+++ api-gateway/src/main/java/org/demo/apigateway/utils/JwtAuthenticationFilter.java
@@ -0,0 +1,56 @@
+package org.demo.apigateway.utils;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import java.security.KeyStore;
+import java.security.PublicKey;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+@Component
+public class JwtAuthenticationFilter implements WebFilter {
+  private static final String KEYSTORE_FILE = "jwtkeystore.jks";
+  private static final String KEY_ALIAS = "jwtkey";
+  private static final String KEYSTORE_PASSWORD = "password";
+  private final KeyStore keyStore;
+
+  public JwtAuthenticationFilter() throws Exception {
+    keyStore = KeyStore.getInstance(KeyStore.getDefaultType());
+    try (var inputStream = getClass().getClassLoader().getResourceAsStream(KEYSTORE_FILE)) {
+      keyStore.load(inputStream, KEYSTORE_PASSWORD.toCharArray());
+    }
+  }
+
+  private PublicKey getPublicKey() throws Exception {
+    return keyStore.getCertificate(KEY_ALIAS).getPublicKey();
+  }
+
+
+  @Override
+  public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+    String authHeader = exchange.getRequest().getHeaders().getFirst("Authorization");
+
+    if (authHeader == null || !authHeader.startsWith("Bearer")) {
+      exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+      return exchange.getResponse().setComplete();
+    }
+
+    String token = authHeader.substring(7);
+
+    try {
+      Claims claims = Jwts.parser()
+          .setSigningKey(getPublicKey())
+          .parseClaimsJws(token)
+          .getBody();
+      exchange.getAttributes().put("claims", claims);
+    } catch (Exception e) {
+      exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+      return exchange.getResponse().setComplete();
+    }
+    return chain.filter(exchange);
+  }
+}
